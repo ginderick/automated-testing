d0aa25e055856a014e8034b8e89414906f7e1d2d
ginderick <ginderick@gmail.com>
2024-04-25 17:38:40 +0800

    add s3 resources

--- main.tf
+++ main.tf
@@ -2,3 +2,25 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-test-bucket-ginderick123"
+
+  tags = {
+    Name        = "my-test-bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_object" "new_upload" {
+  bucket = aws_s3_bucket.test_bucket.id
+  key    = "new_upload/"
+
+}
+
+resource "aws_s3_object" "for_processing" {
+  bucket = aws_s3_bucket.test_bucket.id
+  key    = "for_processing/"
+
+}
+
